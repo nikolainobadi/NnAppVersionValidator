6162f0614be77a45ee62b3ac5a631383b1f711ef
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2023-04-29 12:29:27 -0700

    added comments to API file for clarification

--- Sources/NnAppVersionValidator/API/NnAppVersionValidator.swift
+++ Sources/NnAppVersionValidator/API/NnAppVersionValidator.swift
@@ -1,11 +1,19 @@
 import Foundation
 
+/// Method will return bool based on whether App Version on Device is lower than App Version on AppStore
 public protocol NnVersionValidator {
     func checkIfVersionUpateIsRequired() async throws -> Bool
 }
 
+/// Composer method to create NnVersionValidator object
+/// infoDictionary is used to parse app version number on device
+/// bundleId is used to make network request to Apple to fetch AppStore version number
+/// selectedVersionNumber allows you to choose which type of update to check
+///  - example Version number: 2.3.4
+///     - .major: returns true if first number in VersionNumber has changed
+///     - .minor: returns true if first OR second number in VersionNumber has changed
+///     - .patch: returns true if any number in the VersionNumber has changed
 public func makeVersionValidator(infoDictionary: [String: Any]?, bundleId: String?, selectedVersionNumber: VersionNumberType = .major) -> NnVersionValidator {
-    
     let localLoader = LocalVersionNumberLoader(infoDictionary: infoDictionary)
     let remoteLoader = RemoteVersionNumberLoader(bundleId: bundleId)
     

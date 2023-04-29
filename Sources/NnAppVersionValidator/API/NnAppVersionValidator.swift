40a25c737d776a9307b24cd3f2efa90908671eed
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2023-04-29 12:20:36 -0700

    moved LocalVersionNumberLoader to its own file

--- Sources/NnAppVersionValidator/API/NnAppVersionValidator.swift
+++ Sources/NnAppVersionValidator/API/NnAppVersionValidator.swift
@@ -16,26 +16,7 @@ public protocol NnVersionValidator {
 
 
 
-final class LocalVersionNumberLoader {
-    private let infoDictonary: [String: Any]?
-    private let versionStringId = "CFBundleShortVersionString"
-    
-    init(infoDictionary: [String: Any]?) {
-        self.infoDictonary = infoDictionary
-    }
-}
 
-
-// MARK: - Loader
-extension LocalVersionNumberLoader: VersionNumberLoader {
-    func loadVersionNumber() async throws -> VersionNumber {
-        guard let deviceVersionString = infoDictonary?[versionStringId] as? String else {
-            throw VersionValidationError.missingDeviceVersionString
-        }
-
-        return try VersionNumberMapper.map(deviceVersionString)
-    }
-}
 
 
 

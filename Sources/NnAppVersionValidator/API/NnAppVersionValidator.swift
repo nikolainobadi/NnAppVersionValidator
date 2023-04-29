71de4923f7f8179dc8ca53d67fddbf66cc432ba2
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2023-04-29 12:21:18 -0700

    moved RemoteVersionNumberLoader to its own file

--- Sources/NnAppVersionValidator/API/NnAppVersionValidator.swift
+++ Sources/NnAppVersionValidator/API/NnAppVersionValidator.swift
@@ -30,35 +30,3 @@ enum VersionValidationError: Error {
     case missingDeviceVersionString
     case unableToFetchVersionFromAppStore
 }
-
-final class RemoteVersionNumberLoader {
-    private let bundleId: String?
-    
-    public init(bundleId: String?) {
-        self.bundleId = bundleId
-    }
-}
-
-
-// MARK: - Loader
-extension RemoteVersionNumberLoader: VersionNumberLoader {
-    func loadVersionNumber() async throws -> VersionNumber {
-        guard let bundleId = bundleId,
-              let url = URL(string: "https://itunes.apple.com/lookup?bundleId=\(bundleId)")
-        else {
-            throw VersionValidationError.invalidBundleId
-        }
-        
-        let (data, _) = try await URLSession.shared.data(from: url)
-        
-        guard let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any],
-              let results = json["results"] as? [[String: Any]],
-              let versionString = results.first?["results"] as? String
-        else {
-            throw VersionValidationError.missingDeviceVersionString
-        }
-        
-        return try VersionNumberMapper.map(versionString)
-    }
-}
-

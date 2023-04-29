d7f450b122f36e9c8d137188cc4076f27befffd4
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2023-04-29 12:40:53 -0700

    added first unit test to verify no update is required if versionNumbers match

--- Tests/NnAppVersionValidatorTests/NnAppVersionValidatorTests.swift
+++ Tests/NnAppVersionValidatorTests/NnAppVersionValidatorTests.swift
@@ -2,7 +2,45 @@ import XCTest
 @testable import NnAppVersionValidator
 
 final class NnAppVersionValidatorTests: XCTestCase {
-    func testExample() throws {
+    func test_checkIfVersionUpateIsRequired_noUpdateRequired() async throws {
+        let updateRequired = try await makeSUT().checkIfVersionUpateIsRequired()
         
+        XCTAssertFalse(updateRequired)
+    }
+}
+
+
+// MARK: - SUT
+extension NnAppVersionValidatorTests {
+    func makeSUT(deviceVersion: VersionNumber? = nil, onlineVersion: VersionNumber? = nil, selectedVersionNumber: VersionNumberType = .major, throwLocalError: Bool = false, throwRemoteError: Bool = false) -> VersionValidator {
+        
+        let localLoader = MockLoader(throwError: throwLocalError, versionNumber: deviceVersion ?? makeVersionNumber())
+        let remoteLoader = MockLoader(throwError: throwRemoteError, versionNumber: onlineVersion ?? makeVersionNumber())
+        
+        return VersionValidator(local: localLoader, remote: remoteLoader, selectedVersionNumber: selectedVersionNumber)
+    }
+}
+
+
+// MARK: - Helpers
+extension NnAppVersionValidatorTests {
+    func makeVersionNumber(_ major: Int = 1, minor: Int = 0, patch: Int = 0) -> VersionNumber {
+        return VersionNumber(majorNum: major, minorNum: minor, patchNum: patch)
+    }
+    
+    class MockLoader: VersionNumberLoader {
+        let throwError: Bool
+        let versionNumber: VersionNumber
+        
+        init(throwError: Bool, versionNumber: VersionNumber) {
+            self.throwError = throwError
+            self.versionNumber = versionNumber
+        }
+        
+        func loadVersionNumber() async throws -> VersionNumber {
+            if throwError { throw NSError(domain: "Test", code: 0) }
+            
+            return versionNumber
+        }
     }
 }

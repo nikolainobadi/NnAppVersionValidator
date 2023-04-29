dfbc5c18cff6fbd2829a0e3a01b8b1743354e3d3
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2023-04-29 11:41:11 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# NnAppVersionValidator
+
+A description of this package.
---
+++ Sources/NnAppVersionValidator/NnAppVersionValidator.swift
@@ -0,0 +1,6 @@
+public struct NnAppVersionValidator {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}

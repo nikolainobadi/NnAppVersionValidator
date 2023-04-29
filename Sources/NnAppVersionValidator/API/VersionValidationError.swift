896089e499d504e3bd6430ec051b411361de597d
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2023-04-29 12:32:10 -0700

    added extension to VersionValidationError for messages

--- Sources/NnAppVersionValidator/API/VersionValidationError.swift
+++ Sources/NnAppVersionValidator/API/VersionValidationError.swift
@@ -13,3 +13,14 @@ public enum VersionValidationError: Error {
     case missingDeviceVersionString
     case unableToFetchVersionFromAppStore
 }
+
+public extension VersionValidationError {
+    var message: String {
+        switch self {
+        case .missingNumber: return "A number is missing from the version number, most likely due to a decoding error"
+        case .invalidBundleId: return "The bundleId is not valid or was not provided"
+        case .missingDeviceVersionString: return "The version number was unable to be decoded, either from the device or from Apple"
+        case .unableToFetchVersionFromAppStore: return "A network error occured while attempting to fetch the version number from Apple"
+        }
+    }
+}
